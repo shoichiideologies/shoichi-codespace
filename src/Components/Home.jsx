fc1dc5b3d631418892ccf639c328f879d1662a2c
Angelo Miranda <114156097+shoichiideologies@users.noreply.github.com>
2024-10-17 16:26:27 +0000

    relatives

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "..src/images/top.jpeg";
+import image from "..images/top.jpeg";
 
 const imageAltText = "Angelo Miranda";
 
